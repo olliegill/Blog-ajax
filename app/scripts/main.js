836973c82b9871633a01fb024b157a08edf6efc5
Ollie Gill <olliegill87@googlemail.com>
2014-09-16 22:06:13 -0400

    commit with failed real time

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,15 @@ function blogTemplate(templateId, container, model) {
   $(container).append(renderTemplate);
 }
 
+function editTemplate(templateId, container, model) {
+  var templateString = $('#' + templateId).text();
+  var templateFunction = _.template(templateString);
+  var renderTemplate = templateFunction(model);
+  console.log("ID: "+templateId, renderTemplate);
+  $(container).append(renderTemplate);
+}
+
+
 //POST
 
 $("form").submit(function(event){
@@ -16,23 +25,8 @@ $.ajax("http://tiny-pizza-server.herokuapp.com/collections/olliesblog", {
   type: "POST",
   dataType: 'json',
   data: blog
-}).done(function(){
-    //$(".blog-repo").html("");
 
-     $.ajax("http://tiny-pizza-server.herokuapp.com/collections/olliesblog", {
-       type: "GET",
-       dataType: "json",
-     }).done(function(data){
-        _.each(data, function(getblog){
-       var blogStuff = {
-         title: getblog.title,
-         text: getblog.text,
-         id: getblog._id,
-       };
 
-       blogTemplate('blogId', '.each-post', blogStuff);
-     });
-     });
 });
 });
 
@@ -71,9 +65,42 @@ $('.each-post').delegate('.delete', 'click', function(){
 
 //PUT
 
-// -click edit.
-// -open textarea.
-// -submit new text back into post.
+$('.edit').on('click', function(editWindow){
+  $()
+    //toggleClass somewhere
+    // editTemplate('edit-window', '.each-post', );
+});
+
+
+
+
+
+
+
+// $('.each-post').delegate('.edit', 'click', function(){
+//
+//   $.ajax("http://tiny-pizza-server.herokuapp.com/collections/olliesblog/" + $(this).attr('data-id'), {
+//    type: "PUT",
+//    dataType: 'json',})
+//    .done(function(editData){
+//
+//      //if('title' == $(this).title){
+//
+//      //clear html
+//
+//      _.each(data, function(editblog){
+//     var blogEdit = {
+//       title: editblog.title,
+//       text: editblog.text,
+//       id: editblog._id,
+//     };
+//
+//     blogTemplate('blogId', '.each-post', blogEdit);
+//
+//    });
+//  });
+//
+// });
 
 
 //HEADER
